4cf688be4c21cf2a895ee70259f6d160797bb24f
Equim <791457156@qq.com>
2016-12-12 14:45:52 +0800

    Create P1756 数字反转.c

---
+++ Vijos/P1756 数字反转.c
@@ -0,0 +1,29 @@
+// P1756 数字反转
+// https://vijos.org/p/1756
+// by Equim
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char N[20],*p=N;
+	int flag=1;
+	gets(N);
+	if(N[0]=='-')
+		printf("-");
+	strrev(N);
+	while(flag)
+	{
+		if(*p!='0')
+			flag=0;
+		else
+			p++;
+	}
+	while(*p!='\0')
+	{
+		if(*p!='-')
+			printf("%c",*p);
+		p++;
+	}
+	return 0;
+}
