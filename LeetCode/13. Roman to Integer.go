734842052e343b0dd8224e5185921a3a47cd447c
Equim <sayaka@ekyu.moe>
2017-04-06 01:25:42 +0800

    Update on 17-04-06

--- LeetCode/13. Roman to Integer.go
+++ LeetCode/13. Roman to Integer.go
@@ -5,17 +5,17 @@
 func romanToInt(s string) int {
     romans := map[string]int{"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
-    ret, prev := 0, "M"
+    ret, prev := 0, 1000
     for i := 0; i < len(s); i++ {
-        digit := string(s[i])
+        digit := romans[string(s[i])]
 
-        switch romans[digit] / romans[prev] {
+        switch digit / prev {
         case 5:
-            ret += 3 * romans[prev]
+            ret += 3 * prev
         case 10:
-            ret += 8 * romans[prev]
+            ret += 8 * prev
         default:
-            ret += romans[digit]
+            ret += digit
         }
 
         prev = digit
